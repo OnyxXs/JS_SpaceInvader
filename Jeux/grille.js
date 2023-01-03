37873b760db67ee5dc64fa2097c6bb310d3e284b
OnyxXs <timeo.donnaes@gmail.com>
2023-01-03 14:35:39 +0100

    grille

---
+++ Jeux/grille.js
@@ -0,0 +1,8 @@
+var container = document.querySelector(".grille");
+for (let i=0; i<240; i++){
+const bloc = document.createElement('div');
+            container.appendChild(bloc);
+            //indexAttr++;
+
+ console.log(i);
+}
